548986333847096cb3b3c82a4f95d63324aaeda7
jonathandry32 <bayronkevin1@gmail.com>
2023-07-10 11:18:53 +0200

    script

--- script.sql
+++ script.sql
@@ -1,9 +1,8 @@
-drop database system_info;
-create database system_info;
-use system_info;
+create database examen48h_base;
+use examen48h_base;
 
 create table genre(
-    idGenre int auto_increment primary_key,
+    idGenre int auto_increment primary key,
     nom varchar(15)
 );
 insert into genre(nom) values('Homme');
@@ -23,11 +22,11 @@ create table utilisateur(
 );
 
 -- admin 1 oui 0 non
-insert into utilisateur(email,passwrd,username,numero,adresse,isAdmin) values ('Admin@admin.net','root','Administrateur','+261 20 22 456 12','Local',1);
-insert into utilisateur(email,passwrd,username,numero,adresse,isAdmin) values ('lol@bot.io','lol','Bot','+261 34 52 558 91','Lot Iv 7 Ivato',0);
-insert into utilisateur(email,passwrd,username,numero,adresse,isAdmin) values ('jonathan@gmail.com','jonathan','Jonathandry32','+261 33 125 78','Lot II C 1AC Manjakaray',0);
-insert into utilisateur(email,passwrd,username,numero,adresse,isAdmin) values ('koloina@gmail.com','koloina','Koloina06','+261 32 54 789 12','Lot Ter Ambohijanaka',0);
-insert into utilisateur(email,passwrd,username,numero,adresse,isAdmin) values ('johary@gmail.com','johary','Johary17','+261 34 78 546 12','Lot domaine Ampefiloha',0);
+insert into utilisateur(email,passwrd,username,genre,numero,adresse,isAdmin) values ('Admin@admin.net','root','Administrateur',1,'+261 20 22 456 12','Local',1);
+insert into utilisateur(email,passwrd,username,genre,numero,adresse,isAdmin) values ('lol@bot.io','lol','Bot',2,'+261 34 52 558 91','Lot Iv 7 Ivato',0);
+insert into utilisateur(email,passwrd,username,genre,numero,adresse,isAdmin) values ('jonathan@gmail.com','jonathan','Jonathandry32',1,'+261 33 125 78','Lot II C 1AC Manjakaray',0);
+insert into utilisateur(email,passwrd,username,genre,numero,adresse,isAdmin) values ('koloina@gmail.com','koloina','Koloina06',2,'+261 32 54 789 12','Lot Ter Ambohijanaka',0);
+insert into utilisateur(email,passwrd,username,genre,numero,adresse,isAdmin) values ('johary@gmail.com','johary','Johary17',1,'+261 34 78 546 12','Lot domaine Ampefiloha',0);
 
 create table detail_utilisateur(
     idUtilisateur int,
@@ -37,13 +36,73 @@ create table detail_utilisateur(
 	foreign key (idUtilisateur) references utilisateur(idUtilisateur)
 );
 
-create table objet (
-	idobjet int auto_increment primary key,
-	idcategorie int,
-	name varchar(30),
-	proprietaire int,
-	prix double,
-	description varchar(50),
-	foreign key (idcategorie) references categorie(idcategorie),
-	foreign key (proprietaire) references membre(idmembre)
+create table type_objectif(
+    idTypeObjectif int auto_increment primary key,
+    nom varchar(50)
+);
+insert into type_objectif(nom) values('Perdre du poids');
+insert into type_objectif(nom) values('Gagner du poids');
+
+create table regime(
+    idRegime int auto_increment primary key,
+    nom varchar(50),
+    duree double,
+    prix double
+);
+insert into regime(nom,duree) values('Startup',7);
+insert into regime(nom,duree) values('Basique',14);
+insert into regime(nom,duree) values('Rapide',30);
+
+create table activite(
+    idActivite int auto_increment primary key,
+    nom varchar(50)
+);
+insert into activite(nom) values('Corde a saute');
+insert into activite(nom) values('Footing');
+insert into activite(nom) values('Yoga');
+insert into activite(nom) values('Pompe');
+insert into activite(nom) values('Abdo');
+insert into activite(nom) values('Squat');
+
+create table detail_regime(
+    idRegime int,
+    idPlat int,
+    idActivite int,
+	foreign key (idRegime) references regime(idRegime)
+);
+
+create table type_plat(
+    idTypePlat int auto_increment primary key,
+    nom varchar(50)
+);
+insert into type_plat(nom) values ('PetitDej');
+insert into type_plat(nom) values ('Dejeuner');
+insert into type_plat(nom) values ('Gouter');
+insert into type_plat(nom) values ('Diner');
+
+create table plat(
+    idPlat int auto_increment primary key,
+    nom varchar(50),
+    idTypePlat int,
+    prix double,
+    picture varchar(255),
+	foreign key (idTypePlat) references type_plat(idTypePlat)
+);
+
+create table objectif_utilisateur(
+    idObjectif int auto_increment primary key,
+    type_objectif int,
+    minValue double,
+    maxValue double,
+    idRegime int,
+	foreign key (idRegime) references regime(idRegime)
+);
+
+create table regime_utilisateur(
+    idRegimeUtilisateur int auto_increment primary key,
+    idUtilisateur int,
+    idObjectif int,
+    debut Date,
+    fin Date,
+	foreign key (idUtilisateur) references utilisateur(idUtilisateur)
 );
