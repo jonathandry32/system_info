2aa1e97160c7c61085941050426dba24d2932ae3
koloina06 <koloina.mihajatiana@gmail.com>
2023-07-10 15:05:10 +0300

    regime

---
+++ application/models/Regime_model.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Regime_model extends CI_Model
+{
+    public function insertRegime($nom,$duree,$prix){
+        $sql="INSERT INTO regime(nom,duree,prix) values (%s,%d,%d)";
+        $sql=sprintf($sql,$this->db->escape($nom),$duree,$prix);
+        $this->db->query($sql);
+    }
+
+    public function listeRegime(){
+        $query= $this->db->query('SELECT * FROM regime');
+        $data=array();
+        foreach($query->result_array() as $row){
+            array_push($data,$row);
+        }
+        return $data;
+    }
+
+    public function updateRegime($idRegime,$nom,$duree,$prix){
+        $sql='UPDATE regime set nom=%s, duree=%d, prix=%d where idRegime=%d';:
+        $sql=sprintf($sql,$this->db->escape($nom),$duree,$prix,$idRegime);
+        $this->db->query($sql);
+    }
+
+    public function deleteRegime($idRegime){
+        $sql='DELETE FROM regime where idRegime=%d';
+        $sql=sprintf($sql,$idRegime);
+        $this->db->query($sql);
+    }
+}
---
+++ application/views/regime/listeRegime.php
@@ -0,0 +1,53 @@
+<main>
+    <br>
+	<section class="section">
+        <br>
+		<div class="container">
+            <br>
+            <div class="row justify-content-center" style="background-color:whitesmoke;border-color:whitesmoke ">
+                <br>
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Regime</th>
+                <th scope="col">Duree</th>
+                <th scope="col">Modifier</th>
+                <th scope="col">Supprimer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 0; $i < count($list); $i++) {
+            ?>
+                <tr>
+                    <td><?php echo $list[$i]['nom']; ?></td>
+                    <td><?php echo $list[$i]['duree']; ?></td>
+                    <td>
+                        <form action="<?php echo base_url('Regime_controller/updatePage'); ?>" method="post">
+                            <input type="hidden" name="idRegime" value="<?php echo $list[$i]['idRegime']; ?>">
+                            <input type="hidden" name="nom" value="<?php echo $list[$i]['nom']; ?>">
+                            <input type="hidden" name="duree" value="<?php echo $list[$i]['duree']; ?>">
+                            <input type="hidden" name="prix" value="<?php echo $list[$i]['prix']; ?>">
+                            <button type="submit" class="btn btn-success" style="color:white"><i class="bi bi-pencil-square"></i></button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="<?php echo base_url('Regime_controller/delete'); ?>" method="post">
+                            <input type="hidden" name="idRegime" value="<?php echo $list[$i]['idRegime']; ?>">
+                            <button type="submit" class="btn btn-danger" style="color:white"><i class="bi bi-trash-fill"></i></button>
+                        </form>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+    <hr>
+</body>
+</div>
+</div>
+</section>
+</main>
+<br>
